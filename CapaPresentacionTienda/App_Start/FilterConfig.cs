b0e99a57c7dfc823c40d132a64335dadbbac5dc1
sans232 <sansypapyrus95@gmail.com>
2024-12-15 17:43:56 -0400

    proyecto

---
+++ CapaPresentacionTienda/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CapaPresentacionTienda
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CapaPresentacionTienda/Controllers/AccesoController.cs
@@ -0,0 +1,230 @@
+﻿using CapaEntidad;
+using CapaNegocio;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CapaPresentacionTienda.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+        public ActionResult Reestablecer()
+        {
+            return View();
+        }
+        public ActionResult CambiarClave()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(Cliente objeto)
+        {
+            int resultado;
+            string mensaje = string.Empty;
+
+            ViewData["Nombres"] = string.IsNullOrEmpty(objeto.Nombres) ? "" : objeto.Nombres;
+            ViewData["Apellidos"] = string.IsNullOrEmpty(objeto.Apellidos) ? "" : objeto.Apellidos;
+            ViewData["Correo"] = string.IsNullOrEmpty(objeto.Correo) ? "" : objeto.Correo;
+
+            if (objeto.Clave != objeto.ConfirmarClave)
+            {
+
+                ViewBag.Error = "Las contraseñas no coinciden";
+                return View();
+            }
+            if (!AlgoritmoContraseñaSegura(objeto.Clave))
+            {
+                ViewBag.Error = "La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número, un carácter especial y tener una longitud mínima de 8 caracteres.";
+                return View();
+            }
+
+            resultado = new CN_Cliente().Registrar(objeto, out mensaje);
+
+            if (resultado > 0)
+            {
+                ViewBag.Error = null;
+                return RedirectToAction("Index", "Acceso");
+            }
+            else
+            {
+                ViewBag.Error = mensaje;
+                return View();
+            }
+
+        }
+
+        private bool AlgoritmoContraseñaSegura(string password)
+        {
+            bool mayuscula = false, minuscula = false, numero = false, carespecial = false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (Char.IsUpper(password, i))
+                {
+                    mayuscula = true;
+                }
+                else if (Char.IsLower(password, i))
+                {
+                    minuscula = true;
+                }
+                else if (Char.IsDigit(password, i))
+                {
+                    numero = true;
+                }
+                else
+                {
+                    carespecial = true;
+                }
+            }
+            if (mayuscula && minuscula && numero && carespecial && password.Length >= 8)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        [HttpPost]
+        public ActionResult Index(string correo, string clave)
+        {
+            Cliente oCliente = null;
+
+            oCliente = new CN_Cliente().Listar().Where(item => item.Correo == correo && item.Clave == CN_Recursos.ConvertirSha256(clave)).FirstOrDefault();
+
+
+            if (oCliente == null)
+            {
+                ViewBag.Error = "Correo o contraseña no son correctas";
+                return View();
+
+            }
+            else
+            {
+
+                if (oCliente.Reestablecer)
+                {
+                    TempData["IdCliente"] = oCliente.IdCliente;
+                    return RedirectToAction("CambiarClave", "Acceso");
+                }
+                else
+                {
+
+                    FormsAuthentication.SetAuthCookie(oCliente.Correo, false);
+
+                    Session["Cliente"] = oCliente;
+
+                    ViewBag.Error = null;
+                    return RedirectToAction("Index", "Tienda");
+
+
+                }
+
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult Reestablecer(string correo)
+        {
+
+
+            Cliente cliente = new Cliente();
+
+            cliente = new CN_Cliente().Listar().Where(item => item.Correo == correo).FirstOrDefault();
+
+            if (cliente == null)
+            {
+                ViewBag.Error = "No se encontro un cliente relacionado a ese correo";
+                return View();
+            }
+
+
+            string mensaje = string.Empty;
+            bool respuesta = new CN_Cliente().ReestablecerClave(cliente.IdCliente, correo, out mensaje);
+
+            if (respuesta)
+            {
+
+                ViewBag.Error = null;
+                return RedirectToAction("Index", "Acceso");
+
+            }
+            else
+            {
+
+                ViewBag.Error = mensaje;
+                return View();
+            }
+
+
+        }
+
+        [HttpPost]
+        public ActionResult CambiarClave(string idcliente, string claveactual, string nuevaclave, string confirmaclave)
+        {
+
+            Cliente oCliente = new Cliente();
+
+            oCliente = new CN_Cliente().Listar().Where(u => u.IdCliente == int.Parse(idcliente)).FirstOrDefault();
+
+            if (oCliente.Clave != CN_Recursos.ConvertirSha256(claveactual))
+            {
+                TempData["IdCliente"] = idcliente;
+                ViewData["vclave"] = "";
+                ViewBag.Error = "La contraseña actual no es correcta";
+                return View();
+            }
+            else if (nuevaclave != confirmaclave)
+            {
+
+                TempData["IdCliente"] = idcliente;
+                ViewData["vclave"] = claveactual;
+                ViewBag.Error = "Las contraseñas no coinciden";
+                return View();
+            }
+            ViewData["vclave"] = "";
+
+
+            nuevaclave = CN_Recursos.ConvertirSha256(nuevaclave);
+
+
+            string mensaje = string.Empty;
+
+            bool respuesta = new CN_Cliente().CambiarClave(int.Parse(idcliente), nuevaclave, out mensaje);
+
+            if (respuesta)
+            {
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+
+                TempData["IdCliente"] = idcliente;
+
+                ViewBag.Error = mensaje;
+                return View();
+            }
+
+        }
+
+
+
+        public ActionResult CerrarSesion()
+        {
+            Session["Cliente"] = null;
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Acceso");
+
+        }
+    }
+}
